4e170e909995c3a53c23e86b4bfa94176b924f23
Felix Lamouroux <felix@iosphere.de>
2016-07-09 02:31:14 +0200

    Add shadow to rounded view

--- ISHPullUp/ISHPullUpRoundedView.h
+++ ISHPullUp/ISHPullUpRoundedView.h
@@ -16,6 +16,16 @@ IB_DESIGNABLE
 @property (nonatomic) IBInspectable CGFloat strokeWidth;
 /// The corner radius is used for the top left and top right corners of the view. Default is 8pt.
 @property (nonatomic) IBInspectable CGFloat cornerRadius;
+
+/// The shadow opacity is used for the drop shadow above the rounded view. Default is 0.25.
+@property (nonatomic) IBInspectable CGFloat shadowOpacity;
+
+/// The shadow radius is used for the drop shadow above the rounded view. Default is 3.
+@property (nonatomic) IBInspectable CGFloat shadowRadius;
+
+/// The shadow color is used for the drop shadow above the rounded view. Default is black.
+@property (nonatomic) IBInspectable UIColor *shadowColor;
+
 @end
 
 /**
